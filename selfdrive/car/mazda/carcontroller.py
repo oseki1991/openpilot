02e50b4828d82da64f9b38672cd6754835fd72e2
Toro <g1000614@gm.pu.edu.tw>
2022-09-01 09:29:22 +0800

    0901

--- selfdrive/car/mazda/carcontroller.py
+++ selfdrive/car/mazda/carcontroller.py
@@ -69,7 +69,7 @@ class CarController():
       ldw = c.hudControl.visualAlert == VisualAlert.ldw
       steer_required = c.hudControl.visualAlert == VisualAlert.steerRequired
       # TODO: find a way to silence audible warnings so we can add more hud alerts
-      steer_required = steer_required and CS.lkas_allowed_speed
+      steer_required = False
       can_sends.append(mazdacan.create_alert_command(self.packer, CS.cam_laneinfo, ldw, steer_required))
 
     # send steering command
